a78626683ee3de56e6814f7b8b7bdd2e256c8ded
Mrinmoy Shib <201355006+MrinmoyShib@users.noreply.github.com>
2026-08-15 15:02:44 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
-import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<div style={{ padding: '2rem', textAlign: 'center' }}>Registration Page Placeholder</div>} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
